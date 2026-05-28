3b876803ff45b39536758bff1084e4bb58601fe2
samantha-englander <samantha-englander@users.noreply.github.com>
2026-05-28 14:36:22 +0000

    deploy: 05979f94e41c145b35f5c57909816563e52b1ad5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "134796777" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "540029031" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
